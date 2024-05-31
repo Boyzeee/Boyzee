8b77a5144b56f0669529cb37abeaf7d862d6d805
Boyzeee <boyzee1412@gmail.com>
2024-05-31 07:50:28 -0700

    Add files via upload

---
+++ category file/allcategory.js
@@ -0,0 +1,6 @@
+const ham = document.querySelector('.hamburg');
+const sideBar = document.querySelector('.side-bar');
+
+ham.addEventListener('click', function(){
+    sideBar.style.display = 'block'
+})
